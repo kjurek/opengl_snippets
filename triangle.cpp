ccd42a835224d5dac5789b0e4cc248a4d2331e1c
kjurek <kjurek123@gmail.com>
2020-03-24 06:50:53 +0100

    add triangle snippet

---
+++ triangle.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+#include <GL/glew.h>
+#include <GL/glut.h>
+
+void render()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+    glVertex2f(-0.5f, -0.5f);
+    glVertex2f(0, 0.5f);
+    glVertex2f(0.5f, -0.5f);
+    glEnd();
+
+    glutSwapBuffers();
+}
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(1024, 768);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("GLEW Test");
+    glutDisplayFunc(render);
+
+    GLenum err = glewInit();
+    if (GLEW_OK != err) {
+        std::cerr << "Error: " << glewGetErrorString(err) << std::endl;
+        return 1;
+    }
+    std::cout << "Status: Using GLEW " << glewGetString(GLEW_VERSION) << std::endl;
+    std::cout << "Status: Using OpenGL " << glGetString(GL_VERSION) << std::endl;
+
+    glutMainLoop();
+}
