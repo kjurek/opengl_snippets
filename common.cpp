b68b3152b4419aa1ca2a687e9ab01935ecff7c5b
kjurek <kjurek123@gmail.com>
2020-03-27 06:15:44 +0100

    add debug info

--- common.cpp
+++ common.cpp
@@ -6,6 +6,19 @@
 #include <GL/glew.h>
 #include <GL/glut.h>
 
+void debug_callback(GLenum source,
+                    GLenum type,
+                    GLuint id,
+                    GLenum severity,
+                    GLsizei length,
+                    GLchar const* message,
+                    void const* userParam)
+{
+    fprintf( stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+            ( type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "" ),
+            type, severity, message );
+}
+
 void create_window(int argc, char **argv, unsigned width, unsigned height, std::string const &title)
 {
     glutInit(&argc, argv);
@@ -13,11 +26,19 @@ void create_window(int argc, char **argv, unsigned width, unsigned height, std::
     glutInitWindowSize(width, height);
     glutInitWindowPosition(100, 100);
     glutCreateWindow(title.c_str());
-
     GLenum err = glewInit();
     if (GLEW_OK != err) {
         throw glewGetErrorString(err);
     }
+
+#ifndef NDEBUG
+    glEnable(GL_DEBUG_OUTPUT);
+    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+    glDebugMessageCallback(
+        debug_callback,
+        nullptr);
+    glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+#endif
 
     std::cout << "Status: Using GLEW " << glewGetString(GLEW_VERSION) << std::endl;
     std::cout << "Status: Using OpenGL " << glGetString(GL_VERSION) << std::endl;
